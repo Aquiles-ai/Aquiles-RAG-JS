3fbb701b955ec2192c8a618b6f1ba5f2e1d4ebc6
F4k3r22 <fredyriveraacevedo13@gmail.com>
2025-08-02 16:02:54 -0600

    Added '/rag/query-rag' and '/rag/drop_index' routes and Conditional Keys API

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import  RedisConPl  from './plugins/redis.js';
 import swagger from '@fastify/swagger';
 import swaggerUI from '@fastify/swagger-ui';
 import cors from '@fastify/middie';
+import { loadAquilesConfig } from './configs/configs.js';
 import { SCHEMA_FIELD_TYPE, SCHEMA_VECTOR_FIELD_ALGORITHM } from 'redis';
 
 
 const fastify = Fastify({
-    logger: true
+    logger: true,
+    ajv: {
+      customOptions: { allErrors: true}
+    }
 })
 
 await fastify.register(swagger, {
@@ -15,6 +19,13 @@ await fastify.register(swagger, {
         openapi: '3.0.0',
         info: {title: 'Aquiles-RAG-js', version: '0.1.0'}
     }
+})
+
+fastify.setErrorHandler((error, request, reply) => {
+  if (error.validation) {
+    fastify.log.error({ validation: error.validation }, 'AJV validation failed')
+  }
+  reply.send(error)
 })
 
 await fastify.register(RedisConPl)
@@ -27,6 +38,31 @@ await fastify.register(cors, {
 await fastify.register(swaggerUI, {
   routePrefix: '/docs',
   uiConfig: { docExpansion: 'list' }
+})
+
+fastify.decorate('verifyApiKey', async (request, reply) => {
+  const configs   = loadAquilesConfig()
+  const validKeys = (configs.allows_api_keys || [])
+                      .filter(k => k && k.trim())
+
+  if (validKeys.length === 0) {
+    return
+  }
+
+  const apiKey = request.headers['x-api-key']
+  if (!apiKey) {
+    reply
+      .code(403)
+      .send({ detail: 'API key missing' })
+    return reply 
+  }
+  if (!validKeys.includes(apiKey)) {
+    reply
+      .code(403)
+      .send({ detail: 'Invalid API key' })
+    return reply 
+  }
+  return
 })
 
 
@@ -59,7 +95,9 @@ fastify.post('/create/index', {
         }
       }
     }
-  }
+  },
+
+  preHandler: fastify.verifyApiKey,
 }, async (request, reply) => {
   const { indexname, embeddings_dim, dtype, delete_the_index_if_it_exists } = request.body;
   const client = fastify.redis;
@@ -138,7 +176,9 @@ fastify.post('/rag/create', {
         }
       }
     }
-  }
+  },
+  preHandler: fastify.verifyApiKey,
+
 }, async (request, reply) => {
   const { index, name_chunk, dtype, chunk_size, raw_text, embeddings } = request.body;
   const client = fastify.redis;
@@ -177,7 +217,7 @@ fastify.post('/rag/create', {
   };
 
   try{
-    await client.hset(key, mapping=mapping);
+    await client.hSet(key, mapping);
     return {status: 'ok', 
     key: key};
   }
@@ -191,6 +231,167 @@ fastify.post('/rag/create', {
 });
 
 
+fastify.post('/rag/query-rag', {
+  schema:{
+    body: {
+      type: 'object',
+      required: ['index', 'embeddings', 'dtype', 'top_k', 'cosine_distance_threshold'],
+      properties: {
+        index: { type: 'string' },
+        embeddings: { type: 'array', items: { type: 'number' } },
+        dtype:      { type: 'string', enum: ['FLOAT32','FLOAT16','FLOAT64'] },
+        top_k: { type: 'number' },
+        cosine_distance_threshold: { type: 'number' }
+      }
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+          total:  { type: 'number' },
+          results: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                name_chunk: { type: 'string' },
+                chunk_id:   { type: 'number' },
+                chunk_size: { type: 'number' },
+                raw_text:   { type: 'string' },
+                score:      { type: 'number' }
+              },
+              required: ['name_chunk','chunk_id','chunk_size','raw_text','score']
+            }
+          }
+        },
+        required: ['status','total','results']
+      }
+    }
+  },
+  preHandler: fastify.verifyApiKey,
+
+}, async (request, reply) => {
+  const { index, embeddings, dtype, top_k, cosine_distance_threshold } = request.body;
+  const client = fastify.redis;
+
+  let typedArray;
+
+  switch (dtype){
+    case 'FLOAT32':
+      typedArray = new Float32Array(embeddings);
+      break;
+
+    case 'FLOAT16':
+      typedArray = new Uint16Array(embeddings);
+      break;
+
+    case 'FLOAT64':
+      typedArray = new Float64Array(embeddings);
+      break;
+
+    default:
+      throw new Error(`dtype ${dtype} not supported`);
+  }
+
+  const embBytes = Buffer.from(typedArray.buffer);
+
+  let similar;
+  try {
+    similar = await client.ft.search(
+      index,                                      
+      `*=>[KNN ${top_k} @embedding $vec AS score]`,
+      {
+        PARAMS: {                                   
+          vec: embBytes
+        },
+        RETURN: [                                  
+          'name_chunk',
+          'chunk_id',
+          'chunk_size',
+          'raw_text',
+        'score'
+        ],
+        DIALECT: '2'                               
+      });
+  }
+  catch(error){
+    return reply
+      .code(500)
+      .send({
+        error: `Search error:  '${error}'`
+      });
+  }
+
+  let docs = similar.documents.map(d => ({
+    name_chunk: d.value.name_chunk,
+    chunk_id:   Number(d.value.chunk_id),
+    chunk_size: Number(d.value.chunk_size),
+    raw_text:   d.value.raw_text,
+    score:      Number(d.value.score)
+  }));
+
+  if (cosine_distance_threshold != null) {
+    docs = docs.filter(d => d.score <= cosine_distance_threshold);
+  }
+
+  docs = docs.slice(0, top_k);
+
+  return reply.code(200).send({
+    status:  'ok',
+    total:   docs.length,
+    results: docs
+  });
+})
+
+fastify.post('/rag/drop_index', {
+  schema:{
+    body: {
+      type: 'object',
+      required: ['index_name', 'delete_docs'],
+      properties: {
+        index_name: { type: 'string' },
+        delete_docs: { type: 'boolean' }
+      }
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+          drop_index:  { type: 'string' },
+        },
+      }
+    }
+  },
+  preHandler: fastify.verifyApiKey,
+
+}, async (request, reply) => {
+  const { index_name, delete_docs } = request.body;
+  const client = fastify.redis;
+
+  let res;
+  try{
+    if (delete_docs){
+      res = await client.ft.dropIndex(index_name, { DD: true });
+    }
+    else{
+      res = await client.ft.dropIndex(index_name);
+    }
+
+    return reply.code(200).send({
+      status:  res,
+      drop_index:   index_name
+    });
+  }
+  catch(error){
+    return reply
+      .code(500)
+      .send({
+        error: `Delete error:  '${error}'`
+      });
+  }
+})
 
 fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
   if (err) {
